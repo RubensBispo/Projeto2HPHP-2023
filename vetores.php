75676c7f6a53cd5efc07d8b759f0c02fe91a89fd
Rubens Bispo <rubensbispo29@gmail.com>
2023-09-13 20:12:51 -0300

    vetoresx em php

---
+++ vetores.php
@@ -0,0 +1,16 @@
+<?php 
+
+	//crinado um vetor de nomes
+	$nomes = array();
+	$nomes[0] = 'José';
+	$nomes[1] = 'João';
+	$nomes[2] = true;
+	$nomes[3] = 'Pedro';
+	$nomes[4] = 20;
+
+
+
+	echo "Imprimindo vetor de nomes ". $nomes[2] . "<br/>";
+
+
+ ?>
