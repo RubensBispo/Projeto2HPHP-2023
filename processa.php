141dbdd30361ae8c6c2c2f28a6e5bc5127ebbef8
Rubens Bispo <rubensbispo29@gmail.com>
2023-10-16 20:12:56 -0300

    Calculadora com funcao

--- processa.php
+++ processa.php
@@ -2,5 +2,51 @@
 	
 	echo "<h1>Bem vindo ao Sistema</h1>";
 	$num1 = $_POST['num1'];
-	echo "$num1";
+	$num2 = $_POST['num2'];
+	$operacao = $_POST["operacao"];
+
+	switch ($operacao) {
+		case 'soma':
+			$resp = soma($num1,$num2);
+			break;
+		case 'subtracao':
+			$resp = subtracao($num1,$num2);
+			break;
+		case 'multiplicacao':
+			$resp = multiplicacao($num1,$num2);
+			break;
+		case 'divisao':
+			if ($num2<=0) {
+				$resp = "impossível divisão por zero";
+			}
+			else{
+				$resp = divisao($num1,$num2);
+			}
+			break;
+		default:
+			// code...
+			break;
+	}
+
+	function soma($num1,$num2)
+	{
+		return($num1 + $num2);
+	}
+
+	function subtracao($num1,$num2)
+	{
+		return($num1 - $num2);
+	}
+
+	function multiplicacao($num1,$num2)
+	{
+		return($num1 * $num2);
+	}
+
+	function divisao($num1,$num2)
+	{
+		return($num1 / $num2);
+	}
+
+	echo "$resp";
  ?>
