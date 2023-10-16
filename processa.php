65c6d2610d71b00eff77ae85193017bc921562b5
Rubens Bispo <rubensbispo29@gmail.com>
2023-10-16 20:17:53 -0300

    Calculadora com funcao

--- processa.php
+++ processa.php
@@ -4,31 +4,31 @@
 	if (!empty($_POST['num1'] && $_POST['num2']))
 	{
 		$num1 = $_POST['num1'];
-	$num2 = $_POST['num2'];
-	$operacao = $_POST["operacao"];
+		$num2 = $_POST['num2'];
+		$operacao = $_POST["operacao"];
 
-	switch ($operacao) {
-		case 'soma':
-			$resp = soma($num1,$num2);
-			break;
-		case 'subtracao':
-			$resp = subtracao($num1,$num2);
-			break;
-		case 'multiplicacao':
-			$resp = multiplicacao($num1,$num2);
-			break;
-		case 'divisao':
-			if ($num2<=0) {
-				$resp = "impossível divisão por zero";
-			}
-			else{
-				$resp = divisao($num1,$num2);
-			}
-			break;
-		default:
-			// code...
-			break;
-	}
+		switch ($operacao) {
+			case 'soma':
+				$resp = soma($num1,$num2);
+				break;
+			case 'subtracao':
+				$resp = subtracao($num1,$num2);
+				break;
+			case 'multiplicacao':
+				$resp = multiplicacao($num1,$num2);
+				break;
+			case 'divisao':
+				if ($num2<=0) {
+					$resp = "impossível divisão por zero";
+				}
+				else{
+					$resp = divisao($num1,$num2);
+				}
+				break;
+			default:
+				// code...
+				break;
+		}
 	}
 	
 
