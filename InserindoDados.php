3d68297f24de933f47c31985ff7d1c1d3ab0bccb
Rubens Bispo <rubensbispo29@gmail.com>
2024-03-11 21:40:04 -0300

    Add files via upload

--- InserindoDados.php
+++ InserindoDados.php
@@ -4,13 +4,58 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Formulario inserindo dados em uma tabela</title>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+
 </head>
+
 <body>
-	<form action="inserir_dados.php" method="post">
-		Nome :<input type="text" name="nome"></p>
-		Sobrenome :<input type="text" name="sobrenome"></p>
-		Idade :<input type="text" name="idade"></p>
-		<input type="submit" name="Enviar">
-	</form>
+
+<h3>Using CSS to style an HTML Form</h3>
+
+<div>
+  <form action="/InserindoDados.php">
+    <label for="nome">Nome</label>
+    <input type="text" id="fnome" name="nome" placeholder="Digite seu nome">
+
+    <label for="sobrenome">Sobrenome</label>
+    <input type="text" id="sobrenome" name="sobrenome" placeholder="Sobrenome">
+
+    
+  
+    <input type="submit" value="Cadastrar">
+  </form>
+</div>
+
 </body>
 </html>
